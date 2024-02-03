690948f58abf7b9e3185bd13db9670975424b67c
gfoliaco <germanfoliaco2@gmail.com>
2024-02-03 12:39:13 -0500

    Front + CSV + Batch + Sqls OK

--- main.py
+++ main.py
@@ -37,6 +37,79 @@ def insert_data(table_name, data):
     finally:
         conn.close()
 
+def Query():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute("""
+    SELECT         
+        d.department AS Department,
+        j.job AS Job,
+        COUNT(CASE WHEN strftime('%m', e.datetime) BETWEEN '01' AND '03' THEN 1 END) AS Q1,
+        COUNT(CASE WHEN strftime('%m', e.datetime) BETWEEN '04' AND '06' THEN 1 END) AS Q2,
+        COUNT(CASE WHEN strftime('%m', e.datetime) BETWEEN '07' AND '09' THEN 1 END) AS Q3,
+        COUNT(CASE WHEN strftime('%m', e.datetime) BETWEEN '10' AND '12' THEN 1 END) AS Q4
+    FROM 
+        hired_employees e
+    JOIN 
+        departments d ON e.department_id = d.id
+    JOIN 
+        jobs j ON e.job_id = j.id
+    WHERE 
+        strftime('%Y', e.datetime) = '2021'
+    GROUP BY 
+        Department, Job
+    ORDER BY 
+        Department ASC, Job ASC;
+    """)
+    # Recuperar los resultados de la consulta
+    rows = cursor.fetchall()
+    # Procesar los resultados
+    for row in rows:
+        st.write(row)
+    # Cerrar la conexión
+    conn.close()
+
+def Query2():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute("""
+    SELECT 
+        d.id AS Department_ID,
+        d.department AS Department_Name,
+        COUNT(e.id) AS Number_of_Employees_Hired
+    FROM 
+        departments d
+    JOIN 
+        hired_employees e ON d.id = e.department_id
+    WHERE 
+        strftime('%Y', e.datetime) = '2021'
+    GROUP BY 
+        d.id, d.department
+    HAVING 
+        COUNT(e.id) > (
+            SELECT 
+                AVG(employees_count)
+            FROM 
+                (SELECT 
+                    COUNT(*) AS employees_count
+                FROM 
+                    hired_employees
+                WHERE 
+                    strftime('%Y', datetime) = '2021'
+                GROUP BY 
+                    department_id)
+        )
+    ORDER BY 
+        Number_of_Employees_Hired DESC;
+
+    """)
+    # Recuperar los resultados de la consulta
+    rows = cursor.fetchall()
+    # Procesar los resultados
+    for row in rows:
+        st.write(row)
+    # Cerrar la conexión
+    conn.close()
 
 # Streamlit app
 def main():
@@ -66,6 +139,15 @@ def main():
     if st.button('Insert Batch'):
         rows = [row.split(',') for row in data_input.split(';')]
         insert_data(table_name, pd.DataFrame(rows))
+
+    if st.button('QUERY SQL'):
+        st.write("department_____job____________Q1__Q2__Q3__Q4")
+        Query()
+
+    if st.button('QUERY SQL2'):
+        st.write("id_____department____hired")
+        Query2()
+    
 
 if __name__ == '__main__':
     main()
